e317d7f70dedbef20343d13489e0cc89352b3589
Antoine Gersant <antoine.gersant@lesforges.org>
2017-11-23 23:08:00 -0800

    Removed unused command

--- game/src/graphics/GFXCommands.lua
+++ game/src/graphics/GFXCommands.lua
@@ -2,9 +2,6 @@ local CLI = require( "src/dev/cli/CLI" );
 local GFXConfig = require( "src/graphics/GFXConfig" );
 
 
-local setZoom = function( zoom )
-	GFXConfig:setZoom( zoom );
-end
 
 local enableFullscreen = function( )
 	GFXConfig:setFullscreenEnabled( true );
@@ -14,6 +11,5 @@ local disableFullscreen = function( )
 	GFXConfig:setFullscreenEnabled( false );
 end
 
-CLI:addCommand( "setZoom zoom:number", setZoom );
 CLI:addCommand( "enableFullscreen", enableFullscreen );
 CLI:addCommand( "disableFullscreen", disableFullscreen );
